be6a4cc9f49a91139e6f956968eb876d65cbd143
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-04-29 16:12:23 +0200

    Move WidgetDataWhiteList handling to ClientScripting project

--- bundles/org.eclipse.rap.addons.dropdown/src/org/eclipse/rap/addons/dropdown/DropDownViewer.java
+++ bundles/org.eclipse.rap.addons.dropdown/src/org/eclipse/rap/addons/dropdown/DropDownViewer.java
@@ -17,9 +17,8 @@ import java.util.List;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.rap.addons.dropdown.internal.resources.ResourceLoaderUtil;
 import org.eclipse.rap.clientscripting.ClientListener;
+import org.eclipse.rap.clientscripting.WidgetDataWhiteList;
 import org.eclipse.rap.rwt.RWT;
-import org.eclipse.rap.rwt.internal.client.WidgetDataWhiteList;
-import org.eclipse.rap.rwt.internal.client.WidgetDataWhiteListImpl;
 import org.eclipse.rap.rwt.lifecycle.WidgetUtil;
 import org.eclipse.rap.rwt.remote.AbstractOperationHandler;
 import org.eclipse.rap.rwt.remote.UniversalRemoteObject;
@@ -161,7 +160,7 @@ public class DropDownViewer {
 
   private void linkClientObjects() {
     // NOTE : Order is relevant, DropDown renders immediately!
-    addWidgetDataKeys( new String[]{ VIEWER_LINK } );
+    WidgetDataWhiteList.addKey( VIEWER_LINK );
     text.setData( VIEWER_LINK, remoteObject.getId() );
     dropDown.setData( VIEWER_LINK, remoteObject.getId() );
     remoteObject.set( DROPDOWN_KEY, WidgetUtil.getId( dropDown ) );
@@ -184,19 +183,6 @@ public class DropDownViewer {
         }
       }
     } );
-  }
-
-  private static void addWidgetDataKeys( String[] keys ) {
-    WidgetDataWhiteListImpl service
-      = ( WidgetDataWhiteListImpl )RWT.getClient().getService( WidgetDataWhiteList.class );
-    String[] currentKeys = service.getKeys() != null ? service.getKeys() : new String[ 0 ];
-    List<String> list = new ArrayList<String>( Arrays.asList( currentKeys ) );
-    for( int i = 0; i < keys.length; i++ ) {
-      if( !list.contains( keys[ i ] ) ) {
-        list.add( keys[ i ] );
-      }
-    }
-    service.setKeys( list.toArray( new String[ list.size() ]) );
   }
 
   UniversalRemoteObject getRemoteObject() {
