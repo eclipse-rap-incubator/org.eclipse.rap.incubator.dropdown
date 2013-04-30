7e2d1591421b4846d7b89ecf0f88634fc5468a64
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-04-30 18:42:21 +0200

    Adjust and update unit tests for viewer
    * Remove tests for functionality that is now inherited from
      ContentViewer such as setInput, setContentProvider, etc.
    * Add test for calling RemoteObject#listen.
    * Add moreunit config

--- tests/org.eclipse.rap.addons.dropdown.test/src/org/eclipse/rap/addons/dropdown/viewer/DropDownViewer_Test.java
+++ tests/org.eclipse.rap.addons.dropdown.test/src/org/eclipse/rap/addons/dropdown/viewer/DropDownViewer_Test.java
@@ -10,29 +10,27 @@
  ******************************************************************************/
 package org.eclipse.rap.addons.dropdown.viewer;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.anyObject;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.stub;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.*;
 import java.util.List;
 
 import org.eclipse.jface.viewers.*;
-import org.eclipse.rap.addons.dropdown.DropDown;
 import org.eclipse.rap.clientscripting.ClientListener;
 import org.eclipse.rap.rwt.RWT;
 import org.eclipse.rap.rwt.internal.client.WidgetDataWhiteList;
+import org.eclipse.rap.rwt.internal.remote.RemoteObjectImpl;
 import org.eclipse.rap.rwt.lifecycle.PhaseId;
 import org.eclipse.rap.rwt.lifecycle.WidgetUtil;
+import org.eclipse.rap.rwt.remote.Connection;
+import org.eclipse.rap.rwt.remote.RemoteObject;
 import org.eclipse.rap.rwt.testfixture.Fixture;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.*;
@@ -44,21 +42,15 @@ public class DropDownViewer_Test {
 
   // TODO : test reading scripts and attaching listener when possible
 
-  private static final List<Integer> INTEGER_LIST = Arrays.asList(
-    Integer.valueOf( 7 ),
-    Integer.valueOf( 14 ),
-    Integer.valueOf( 21 )
-  );
-  private static final String VIEWER_LINK =
-      "org.eclipse.rap.addons.dropdown.viewer.DropDownViewer#viewer";
-  private static String DROPDOWN_KEY = "dropDown";
+  private static final List<Integer> INTEGER_LIST = Arrays.asList( array( 7, 14, 21 ) );
+  private static final String VIEWER_LINK = DropDownViewer.class.getName() + "#viewer";
+  private static final String DROPDOWN_KEY = "dropDown";
   private static final String TEXT_KEY = "text";
-  private static String ELEMENTS_KEY = "elements";
+  private static final String ELEMENTS_KEY = "elements";
   private Display display;
+  private Shell shell;
   private Text text;
   private DropDownViewer viewer;
-  private Shell shell;
-  private DropDown dropDown;
 
   @Before
   public void setUp() {
@@ -75,22 +67,74 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testContructor_CreatesDropDownWithParent() {
-    createViewer();
+  public void testContructor_createsDropDownWithParent() {
+    viewer = new DropDownViewer( text );
+
     assertSame( text, viewer.getDropDown().getParent() );
   }
 
   @Test
-  public void testConstructor_SetEmptyElements() {
-    createViewer();
+  public void testConstructor_setsEmptyElements() {
+    viewer = new DropDownViewer( text );
 
-    List<String> expected = new ArrayList<String>();
-    assertEquals( expected, Arrays.asList( getElements() ) );
+    assertEquals( 0, getClientElements().length );
   }
 
   @Test
-  public void testGetDefaultTextModifyListenerTwice_ReturnsSameInstance() {
-    createViewer();
+  public void testConstructor_linksTextToRemoteObject() {
+    viewer = new DropDownViewer( text );
+
+    assertEquals( viewer.getRemoteObject().getId(), text.getData( VIEWER_LINK ) );
+  }
+
+  @Test
+  public void testConstructor_linksDropDownToRemoteObject() {
+    viewer = new DropDownViewer( text );
+
+    assertEquals( viewer.getRemoteObject().getId(), viewer.getDropDown().getData( VIEWER_LINK ) );
+  }
+
+  @Test
+  public void testConstructor_linksRemoteObjectToDropDown() {
+    viewer = new DropDownViewer( text );
+
+    String expected = WidgetUtil.getId( viewer.getDropDown() );
+    assertEquals( expected, viewer.getRemoteObject().getString( DROPDOWN_KEY ) );
+  }
+
+  @Test
+  public void testConstructor_linksRemoteObjectToText() {
+    viewer = new DropDownViewer( text );
+
+    String expected = WidgetUtil.getId( text );
+    assertEquals( expected, viewer.getRemoteObject().getString( TEXT_KEY ) );
+  }
+
+  @Test
+  public void testConstructor_addsKeysToWidgetDataWhiteList() {
+    viewer = new DropDownViewer( text );
+
+    WidgetDataWhiteList service = RWT.getClient().getService( WidgetDataWhiteList.class );
+    assertTrue( Arrays.asList( service.getKeys() ).contains( VIEWER_LINK ) );
+  }
+
+  @Test
+  public void testConstructor_preventsMultipleViewersOnSameText() {
+    Fixture.fakePhase( PhaseId.PROCESS_ACTION );
+    viewer = new DropDownViewer( text );
+
+    try {
+      new DropDownViewer( text );
+      fail();
+    } catch( IllegalStateException ex ) {
+      // expected
+    }
+  }
+
+  @Test
+  public void testGetTextModifyListener_twiceReturnsSameInstance() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getTextModifyListener();
     ClientListener listener2 = viewer.getTextModifyListener();
 
@@ -99,8 +143,9 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testGetDefaultTextModifyListenerTwice_DifferentViewerReturnSameInstance() {
-    createViewer();
+  public void testGetTextModifyListener_returnSameInstanceForDifferentViewer() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getTextModifyListener();
     DropDownViewer viewer2 = new DropDownViewer( new Text( shell, SWT.NONE ) );
     ClientListener listener2 = viewer2.getTextModifyListener();
@@ -110,20 +155,21 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testGetDefaultTextVerifyListenerTwice_DifferentViewerReturnSameInstance() {
-    createViewer();
+  public void testGetTextVerifyListener_returnSameInstanceForDifferentViewer() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getTextVerifyListener();
     DropDownViewer viewer2 = new DropDownViewer( new Text( shell, SWT.NONE ) );
     ClientListener listener2 = viewer2.getTextVerifyListener();
-
 
     assertNotNull( listener1 );
     assertSame( listener1, listener2 );
   }
 
   @Test
-  public void testGetDefaultTextKeyDownListenerTwice_DifferentViewerReturnSameInstance() {
-    createViewer();
+  public void testGetTextKeyDownListener_returnSameInstanceForDifferentViewer() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getTextKeyDownListener();
     DropDownViewer viewer2 = new DropDownViewer( new Text( shell, SWT.NONE ) );
     ClientListener listener2 = viewer2.getTextKeyDownListener();
@@ -133,8 +179,9 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testGetDefaultDropDownSelectionListenerTwice_DifferentViewerReturnSameInstance() {
-    createViewer();
+  public void testGetDropDownSelectionListener_returnSameInstanceForDifferentViewer() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getDropDownSelectionListener();
     DropDownViewer viewer2 = new DropDownViewer( new Text( shell, SWT.NONE ) );
     ClientListener listener2 = viewer2.getDropDownSelectionListener();
@@ -144,8 +191,9 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testGetDefaultDropDownDefaultSelectionListenerTwice_DifferentViewerReturnSameInstance() {
-    createViewer();
+  public void testGetDropDownDefaultSelectionListener_returnSameInstanceForDifferentViewer() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getDropDownDefaultSelectionListener();
     DropDownViewer viewer2 = new DropDownViewer( new Text( shell, SWT.NONE ) );
     ClientListener listener2 = viewer2.getDropDownDefaultSelectionListener();
@@ -155,8 +203,9 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testGetDefaultDropDownShowListenerTwice_DifferentViewerReturnSameInstance() {
-    createViewer();
+  public void testGetDropDownShowListener_returnSameInstanceForDifferentViewer() {
+    viewer = new DropDownViewer( text );
+
     ClientListener listener1 = viewer.getDropDownShowListener();
     DropDownViewer viewer2 = new DropDownViewer( new Text( shell, SWT.NONE ) );
     ClientListener listener2 = viewer2.getDropDownShowListener();
@@ -166,84 +215,34 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testLinkTextToRemoteObject() {
-    createViewer();
-
-    assertEquals( viewer.getRemoteObject().getId(), text.getData( VIEWER_LINK ) );
-  }
-
-  @Test
-  public void testLinkDropDownToRemoteObject() {
-    createViewer();
-
-    assertEquals( viewer.getRemoteObject().getId(), dropDown.getData( VIEWER_LINK ) );
-  }
-
-  @Test
-  public void testLinkRemoteObjectToDropDown() {
-    createViewer();
-
-    String expected = WidgetUtil.getId( dropDown );
-    assertEquals( expected, viewer.getRemoteObject().getString( DROPDOWN_KEY ) );
-  }
-
-  @Test
-  public void testLinkRemoteObjectToText() {
-    createViewer();
-
-    String expected = WidgetUtil.getId( text );
-    assertEquals( expected, viewer.getRemoteObject().getString( TEXT_KEY ) );
-  }
-
-  @Test
-  public void testAddKeysToWidgetDataWhiteList() {
-    WidgetDataWhiteList service = RWT.getClient().getService( WidgetDataWhiteList.class );
-    service.setKeys( new String[ 0 ] );
-
-    createViewer();
-
-    List<String> list = Arrays.asList( service.getKeys() );
-    assertTrue( list.contains( VIEWER_LINK ) );
-  }
-
-  @Test
-  public void testAddKeysToWidgetDataWhiteListAndKeepExistingKeys() {
-    WidgetDataWhiteList service = RWT.getClient().getService( WidgetDataWhiteList.class );
-    service.setKeys( new String[]{ "foo" } );
-
-    createViewer();
-
-    List<String> list = Arrays.asList( service.getKeys() );
-    assertTrue( list.contains( "foo" ) );
-  }
-
-  @Test
-  public void testAddKeysToWidgetDataWhiteListOnlyOnce() {
-    WidgetDataWhiteList service = RWT.getClient().getService( WidgetDataWhiteList.class );
-    service.setKeys( new String[]{ VIEWER_LINK } );
-
-    createViewer();
-
-    List<String> list = Arrays.asList( service.getKeys() );
-    assertEquals( list.lastIndexOf( VIEWER_LINK ), list.indexOf( VIEWER_LINK ) );
-  }
-
-  @Test
-  public void testSetInput_AddsElementsToRemoteObject() {
-    createViewer();
+  public void testInputChanged_addsElementsToRemoteObject() {
+    viewer = new DropDownViewer( text );
+    viewer.setContentProvider( new ArrayContentProvider() );
 
     viewer.setInput( INTEGER_LIST );
 
-    String[] result = getElements();
-    List<String> expected = Arrays.asList( new String[]{ "7", "14", "21"} );
-    assertEquals( expected, Arrays.asList( result ) );
+    assertArrayEquals( array( "7", "14", "21" ), getClientElements() );
   }
 
   @Test
-  public void testSetLabelProvide_UpdatesElements() {
-    createViewer();
+  public void testRefresh_updatesElements() {
+    viewer = new DropDownViewer( text );
+    viewer.setContentProvider( new ArrayContentProvider() );
+    String[] input = array( "a", "b", "c" );
+    viewer.setInput( input );
+    input[1] = "x";
 
-    viewer.setInput( INTEGER_LIST );
+    viewer.refresh();
+
+    assertArrayEquals( array( "a", "x", "c" ), getClientElements() );
+  }
+
+  @Test
+  public void testClientElementsUseLabelProvider() {
+    viewer = new DropDownViewer( text );
+    viewer.setContentProvider( new ArrayContentProvider() );
+    viewer.setInput( array( 1, 2, 3 ) );
+
     viewer.setLabelProvider( new LabelProvider() {
       @Override
       public String getText( Object element ) {
@@ -251,50 +250,27 @@ public class DropDownViewer_Test {
       }
     } );
 
-    String[] result = getElements();
-    List<String> expected = Arrays.asList( new String[]{ "Item 7", "Item 14", "Item 21"} );
-    assertEquals( expected, Arrays.asList( result ) );
+    assertArrayEquals( array( "Item 1", "Item 2", "Item 3" ), getClientElements() );
   }
 
   @Test
   public void testProcessSelectionChangedEvent() {
-    createViewer();
-    final List<SelectionChangedEvent> log = new ArrayList<SelectionChangedEvent>();
+    viewer = new DropDownViewer( text );
+    viewer.setContentProvider( new ArrayContentProvider() );
     viewer.setInput( INTEGER_LIST );
-    viewer.addSelectionChangedListener( new ISelectionChangedListener() {
-      public void selectionChanged( SelectionChangedEvent event ) {
-        log.add( event );
-      }
-    } );
+    ISelectionChangedListener listener = mock( ISelectionChangedListener.class );
+    viewer.addSelectionChangedListener( listener );
 
     Map<String, Object> event = createMap( "index", Integer.valueOf( 2 ) );
     viewer.getRemoteObject().notify( "SelectionChanged", event );
 
-    assertEquals( 1, log.size() );
+    verify( listener ).selectionChanged( any( SelectionChangedEvent.class ) );
   }
 
   @Test
-  public void testAddSelectionChangedListener_AddTwiceOnlyAddsOnce() {
-    createViewer();
-    final List<SelectionChangedEvent> log = new ArrayList<SelectionChangedEvent>();
-    viewer.setInput( INTEGER_LIST );
-    ISelectionChangedListener listener = new ISelectionChangedListener() {
-      public void selectionChanged( SelectionChangedEvent event ) {
-        log.add( event );
-      }
-    };
-
-    viewer.addSelectionChangedListener( listener );
-    viewer.addSelectionChangedListener( listener );
-    Map<String, Object> event = createMap( "index", Integer.valueOf( 2 ) );
-    viewer.getRemoteObject().notify( "SelectionChanged", event );
-
-    assertEquals( 1, log.size() );
-  }
-
-  @Test
-  public void testProcessSelectionChangedEvent_ElementField() {
-    createViewer();
+  public void testProcessSelectionChangedEvent_elementField() {
+    viewer = new DropDownViewer( text );
+    viewer.setContentProvider( new ArrayContentProvider() );
     final List<SelectionChangedEvent> log = new ArrayList<SelectionChangedEvent>();
     viewer.setInput( INTEGER_LIST );
     viewer.addSelectionChangedListener( new ISelectionChangedListener() {
@@ -307,13 +283,24 @@ public class DropDownViewer_Test {
     viewer.getRemoteObject().notify( "SelectionChanged", event );
 
     IStructuredSelection selection = ( IStructuredSelection )log.get( 0 ).getSelection();
-    assertEquals( new Integer( 21 ), selection.getFirstElement() );
+    assertEquals( Integer.valueOf( 21 ), selection.getFirstElement() );
+  }
+
+  @Test
+  public void testAddSelectionChangedListener_callsRemoteObjectListen() {
+    RemoteObject remoteObject = fakeRemoteObject();
+    viewer = new DropDownViewer( text );
+
+    viewer.addSelectionChangedListener( mock( ISelectionChangedListener.class ) );
+
+    verify( remoteObject ).listen( eq( "SelectionChanged" ), eq( true ) );
   }
 
   @Test
   public void testDestroyRemoteObject() {
     Fixture.fakePhase( PhaseId.PROCESS_ACTION );
-    createViewer();
+    viewer = new DropDownViewer( text );
+    viewer.setContentProvider( new ArrayContentProvider() );
 
     viewer.getControl().dispose();
 
@@ -321,112 +308,64 @@ public class DropDownViewer_Test {
   }
 
   @Test
-  public void testCreateViewerTwice() {
-    Fixture.fakePhase( PhaseId.PROCESS_ACTION );
-    createViewer();
-
-    try {
-      createViewer();
-      fail();
-    } catch( IllegalStateException ex ) {
-      // expected
-    }
-  }
-
-  @Test
-  public void testSetContentProvider() {
-    IContentProvider provider = mock( IStructuredContentProvider.class );
-    createViewer();
-
-    viewer.setContentProvider( provider );
-
-    assertSame( provider, viewer.getContentProvider() );
-  }
-
-  @Test
-  public void testSetInput_CallsContentProvider() {
-    createViewer();
-    IStructuredContentProvider provider = mock( IStructuredContentProvider.class );
-    stub( provider.getElements( anyObject() ) ).toReturn( new Object[]{} );
-    viewer.setContentProvider( provider );
+  public void testInputChanged_callsContentProviderWithInput() {
+    viewer = new DropDownViewer( text );
+    IStructuredContentProvider contentProvider = mock( IStructuredContentProvider.class );
+    when( contentProvider.getElements( any() ) ).thenReturn( new Object[ 0 ] );
+    viewer.setContentProvider( contentProvider );
     Object input = new Object();
 
     viewer.setInput( input );
 
-    verify( provider ).getElements( eq( input ) );
+    verify( contentProvider ).getElements( eq( input ) );
   }
 
   @Test
-  public void testSetContentProvider_CallsContentProvider() {
-    createViewer();
-    IStructuredContentProvider provider = mock( IStructuredContentProvider.class );
-    stub( provider.getElements( anyObject() ) ).toReturn( new Object[]{} );
-    viewer.setInput( INTEGER_LIST );
+  public void testRefresh_callsContentProviderWithInput() {
+    viewer = new DropDownViewer( text );
+    IStructuredContentProvider contentProvider = mock( IStructuredContentProvider.class );
+    when( contentProvider.getElements( any() ) ).thenReturn( new Object[ 0 ] );
+    viewer.setContentProvider( contentProvider );
+    Object input = new Object();
+    viewer.setInput( input );
+    reset( contentProvider );
 
-    viewer.setContentProvider( provider );
+    viewer.refresh();
 
-    verify( provider, times( 1 ) ).getElements( eq( INTEGER_LIST ) );
-  }
-
-  @Test
-  public void testSetInput_CallsInputChanged() {
-    createViewer();
-    viewer.setInput( INTEGER_LIST );
-    IStructuredContentProvider provider = mock( IStructuredContentProvider.class );
-    stub( provider.getElements( anyObject() ) ).toReturn( new Object[]{} );
-    viewer.setContentProvider( provider );
-    List<Integer> newList = new ArrayList<Integer>();
-
-    viewer.setInput( newList );
-
-    verify( provider ).inputChanged( same( viewer ), eq( INTEGER_LIST ), eq( newList ) );
-  }
-
-  @Test
-  public void testCallDisposeOnContentProvide() {
-    Fixture.fakePhase( PhaseId.PROCESS_ACTION );
-    createViewer();
-    IStructuredContentProvider provider = mock( IStructuredContentProvider.class );
-    viewer.setContentProvider( provider );
-
-    viewer.getControl().dispose();
-
-    verify( provider ).dispose();
+    verify( contentProvider ).getElements( eq( input ) );
   }
 
   //////////
   // Helpers
 
-  private void createViewer() {
-    viewer = new DropDownViewer( text );
-    dropDown = viewer.getDropDown();
-    viewer.setContentProvider( new MyContentProvider() );
-    viewer.setLabelProvider( new LabelProvider() );
-  }
-
-  private String[] getElements() {
+  private String[] getClientElements() {
     return ( String[] )viewer.getRemoteObject().get( ELEMENTS_KEY );
   }
 
-  private Map<String, Object> createMap( String key, Object value ) {
+  private static RemoteObject fakeRemoteObject() {
+    RemoteObject remoteObject = mock( RemoteObjectImpl.class );
+    Connection connection = mock( Connection.class );
+    when( connection.createRemoteObject( anyString() ) ).thenReturn( remoteObject );
+    Fixture.fakeConnection( connection );
+    return remoteObject;
+  }
+
+  private static Map<String, Object> createMap( String key, Object value ) {
     Map<String, Object> event = new HashMap<String, Object>();
     event.put( key, value );
     return event;
   }
 
-  private static class MyContentProvider implements IStructuredContentProvider {
-
-    public void dispose() {
+  private static Integer[] array( int... values ) {
+    Integer[] result = new Integer[ values.length ];
+    for( int i = 0; i < result.length; i++ ) {
+      result[ i ] = Integer.valueOf( values[ i ] );
     }
+    return result;
+  }
 
-    public void inputChanged( Viewer viewer, Object oldInput, Object newInput ) {
-    }
-
-    @SuppressWarnings( "unchecked" )
-    public Object[] getElements( Object inputElement ) {
-      return ( ( List<Integer> )inputElement ).toArray();
-    }
-
+  private static String[] array( String... values ) {
+    return values;
   }
 
 }
