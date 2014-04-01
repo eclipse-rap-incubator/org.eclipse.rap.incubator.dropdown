0785ec761142245b5a6f96e3906053fd4e6921aa
Ivan Furnadjiev <ivan@eclipsesource.com>
2014-04-01 22:45:04 +0300

    Make DropDown shadow themeable
    This change is needed to correspond to DropDown appearance support -
    change 24263.

--- bundles/org.eclipse.rap.addons.dropdown/src/org/eclipse/rap/addons/dropdown/internal/dropdownkit/DropDown.appearances.js
+++ bundles/org.eclipse.rap.addons.dropdown/src/org/eclipse/rap/addons/dropdown/internal/dropdownkit/DropDown.appearances.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 EclipseSource and others.
+ * Copyright (c) 2013, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,8 @@ var appearances = {
     style : function( states ) {
       var tv = new rwt.theme.ThemeValues( states );
       return {
-        border : tv.getCssBorder( "DropDown", "border" )
+        border : tv.getCssBorder( "DropDown", "border" ),
+        shadow : tv.getCssShadow( "DropDown", "box-shadow" )
       };
     }
   }
